a8d52ff80c5187d4a9c6c61a0442b1aae7145677
laxmi-chandana <36924060+laxmi-chandana@users.noreply.github.com>
2020-09-25 13:03:35 -0700

    Protfolio-React

---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/laxmi-chandana",
+    "name": "Lakshmi Avvaru",
+    "role": "Software Engineer and Web Developer",
+    "linkedinId":"Lakshmi Avvaru",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Currently pursuing Masters in Computer Science at California State University and expected graduation date is May 2021.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/lakshmi-avvaru/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/laxmi-chandana",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"Effective collaborator who works well with multiple teams and is adept at working within ambiguous environments and with undefined challenges through strong problem-solving skills and effective communication. Proficient in multiple programming languages and has vast experience with various tools and technologies and is eager to learn more within an Internship.",
+    "address":"California",
+    "phone":"+1(559)394-1249",
+    "email":"alaxmichandana@gmail.com",
+    "education":[
+      {
+        "UniversityName":"California State University, Fresno",
+        "specialization":"Computer Science",
+        "StartDate":"August 2019 - ",
+        "EndDate":"May 2021",
+        "Achievements":"Course Work - Design Analysis and Algorithms, Principles of Programming Languages, Advanced DataBase Management System, Artificial Intelligence, Computer Graphics.",
+      },
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University",
+        "specialization":"Computer Science",
+        "StartDate":"August 2014 - ",
+        "EndDate":"May 2018",
+        "Achievements":"Course Work - Data Structures, Web Technologies, Operating System, Compiler Design, Computer Networking, Computer Architecture."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Tata Consultancy Services",
+        "specialization":"System Engineer",
+        "StartDate":"June 2018 - ",
+        "EndDate":"June 2019",
+        "Achievements":"Implementing MVC architecture for webpages in Infotainment Systems, mainly focusing on Model part using C++. Analyzed the test results to ensure existing functionality and recommend corrective action wherever issues are found. Experienced in Debugging and solving problems during develop and post release. Experienced in object-oriented design, coding, testing patterns, and programming languages like C++and Python. Fixed bugs reported by clients internally filed issues by Product Validation Team. Scrum-based development methodology that includes daily stand-ups, regular sprint review, and retrospectives to discuss work with a cross-functional team using agile development processes. Experienced in communicating with other technical teams, and senior management to collect requirements, describe software product features, technical designs, and product strategy"
+      },
+      {
+        "CompanyName":"Office of Community & Economic Development at Fresno State",
+        "specialization":"Census Worker",
+        "StartDate":"March 2020 -",
+        "EndDate":"September 2020",
+        "Achievements":"Canvass for the Census and also running our Questionnaire Assistance Centers and Kiosks organizing. The locations targeted the communities in and around the CSU Fresno area and the Pinedale Neighborhood."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"python"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JSON"
+      },
+      {
+        "skillname":"MYSQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project - RESORT",
+        "description":"•	Developed a website where a customer can have a overlook of different types of rooms in a beach side resort and information about all those rooms by filtering according to type, size, price, etc. Utilized Reactjs",
+        "imgurl":"images/resort.png",
+        "linkurl" : "https://github.com/laxmi-chandana/ReactJS-Resort-Project"
+      },
+        {
+        "name":"Project - CocktailDB",
+        "description":"•	Developed a React website using hooks likes use useState, useEffect,useRouter and utilized CocktailDB API for data.",
+        "imgurl":"images/CocktailDb.png",
+        "linkurl" : "https://github.com/laxmi-chandana/CocktailsDB"
+      },{
+        "name":"Project - BudgetCalculator",
+        "description":"•	Developed a react-based calculator where a user can add and delete all his expenses.",
+        "imgurl":"images/BudgetCalculator.png",
+        "linkurl" : "https://github.com/laxmi-chandana/BudgetCalculator"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
